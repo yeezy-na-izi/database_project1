32fed1f8e4fb39d90e95d3e1b6e463438c4e664a
yeezy-na-izi <howone1sky@gmail.com>
2023-09-06 10:33:28 +0300

    In database add checker for file

--- database.py
+++ database.py
@@ -1,3 +1,6 @@
+import os
+
+
 class DataBase:
     def __init__(self, name: str, structure: dict[str, (type, bool)]):
         if not name.endswith(".csv"):
@@ -19,16 +22,52 @@ class DataBase:
         self.fields_id = {self.sorted_all_fields[i]: i + 1 for i in range(len(self.sorted_all_fields))}
         self.fields_id['id'] = 0
 
-        # TODO: если файл пустой - записать филды, если нет, проверить на соответствие, если его нет -> удалить все к
-        #  чертям)
+        if (
+                not os.path.exists(self.name) or
+                not os.path.isfile(self.name) or
+                os.stat(self.name).st_size == 0 or
+                not self.check_file_structure()
+        ):
+            self.rewrite_file()
+        else:
+            self.update_index()
 
+    def update_index(self):
+        with open(self.name) as file:
+            file.readline()  # read header
+            file.readline()  # read types
+
+            for record in file:
+                index = int(record.split(',')[0])
+                self.index = max(self.index, index + 1)
+
+    def rewrite_file(self):
+        string = 'id'
+        string_of_types = "<class 'int'>"
+        for field_name in self.sorted_all_fields:
+            string += f',{field_name}'
+            string_of_types += f',{self.structure[field_name][0]}'
+        string += '\n'
+
+        string += string_of_types
+        string += '\n'
         with open(self.name, 'w') as file:
-            string = 'id'
-            for field_name in self.sorted_all_fields:
-                string += f',{field_name}'
-            string += '\n'
-
             file.write(string)
+
+    def check_file_structure(self):
+        with open(self.name) as file:
+            field_names = file.readline().split(',')
+            field_types = file.readline().split(',')
+            field_names[-1] = field_names[-1].strip()
+            field_types[-1] = field_types[-1].strip()
+
+            for i in range(len(field_names)):
+                if field_names[i] not in self.all_fields and field_names[i] != 'id':
+                    return False
+                if field_types[i] != str(self.structure[field_names[i]][0]):
+                    return False
+
+        return True
 
     def generate_required_fields(self):
         for key in self.all_fields:
@@ -84,6 +123,7 @@ class DataBase:
 
         with open(self.name) as file:
             file.readline()  # read header
+            file.readline()  # read types
 
             for record in file:
                 split_record = record.split(',')
@@ -132,6 +172,7 @@ class DataBase:
 
         with open(self.name) as file:
             for_save.append(file.readline())  # read header
+            for_save.append(file.readline())  # read types
 
             for record in file:
                 split_record = record.split(',')
